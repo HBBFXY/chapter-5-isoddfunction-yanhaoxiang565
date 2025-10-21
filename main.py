f072ec1099a40dab02209dd3f705e563be18c4f8
yanhaoxiang565 <ljm060412@qq.com>
2025-10-21 21:20:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ def isOdd(value):
     """
     # 学生实现代码区域
     # 提示：首先检查类型是否为整数，然后检查奇偶性
-    
+   def isOdd(num):
+    return isinstance(num, int) and num % 2 == 1
+print(isOdd(3))  
+print(isOdd(4))   
+print(isOdd(2.5))
+ 
