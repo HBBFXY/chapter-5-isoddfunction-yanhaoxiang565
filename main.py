b1677c6bbdfe58f73f65717856876cbab682ecca
yanhaoxiang565 <ljm060412@qq.com>
2025-10-23 22:26:29 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,7 @@
-def is_prime(num):
-    """判断一个数是否为质数"""
-    if num < 2:
-        return False
-    for i in range(2, int(num ** 0.5) + 1):
-        if num % i == 0:
-            return False
-    return True
-
-def PrimeList(N):
-    """输出小于N的所有质数，以空格分隔"""
-    primes = [str(num) for num in range(2, N) if is_prime(num)]
-    return ' '.join(primes)
-
-# 测试示例
-n = int(input("请输入整数N："))
-print(PrimeList(n))
+def isOdd(n):
+    # 先判断是否是整数（排除bool类型，因为bool是int的子类）
+    if isinstance(n, int) and not isinstance(n, bool):
+        # 是整数，再判断是否为奇数
+        return n % 2 != 0
+    # 非整数或其他情况，返回False
+    return False
